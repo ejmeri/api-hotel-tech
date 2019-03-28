6f2d862a430e406b38e477db81fb17dac2c9f699
felipeamantea <amanteaferreira@gmail.com>
2019-03-28 20:15:05 -0300

    New schemas telephone and address

--- src/configs/db/migration/schemas/index.js
+++ src/configs/db/migration/schemas/index.js
@@ -11,7 +11,10 @@ if (!global.hasOwnProperty('db')) {
         sequelize: sequelize,
 
         Gender: sequelize.import('../../../../contexts/gender/schemas/Gender.js'),
-        Status: sequelize.import('../../../../contexts/status/schemas/Status.js')
+        Status: sequelize.import('../../../../contexts/status/schemas/Status.js'),
+        Country: sequelize.import('../../../../contexts/address/schemas/Country.js'),
+        AddressType: sequelize.import('../../../../contexts/address/schemas/AddressType.js'),
+        TelephoneType: sequelize.import('../../../../contexts/telephone/schemas/TelephoneType.js')
     }
 }
 
