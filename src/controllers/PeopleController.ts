759674af162e0e3d3c28c63337c259b0059ec89c
felipeamantea <amanteaferreira@gmail.com>
2019-04-09 19:30:11 -0300

    cadastro telefone

--- src/controllers/PeopleController.ts
+++ src/controllers/PeopleController.ts
@@ -3,6 +3,8 @@ import { TYPES } from "../config/types";
 import { PeopleService } from "../business/people/services/PeopleService";
 import { Application, Request, Response } from "express";
 import { People } from "../business/people/models/People";
+import { TelephoneService } from "../business/telephone/services/TelephoneService";
+import { TelephoneType } from "../business/telephone/models/TelephoneType";
 
 
 @injectable()
@@ -10,6 +12,9 @@ export class PeopleController {
 
     @inject(TYPES.PeopleService)
     private readonly peopleService: PeopleService;
+
+    @inject(TYPES.TelephoneTypeService)
+    private readonly telephoneService: TelephoneService;
 
     public register(app: Application): void {
         app.route('/people')
@@ -23,6 +28,13 @@ export class PeopleController {
                 this.peopleService.findById(req.params.id)
                     .then(ret => res.send(ret))
                     .catch(err => res.send(err));
-            })
+            });
+        app.route('/telephonetype')
+            .post((req: Request, res: Response) => {
+                this.telephoneService.saveTelephoneType(req.body as TelephoneType)
+                    .then(ret => res.send(ret))
+                    .catch(err => res.send(err));
+            });
+
     }
 }
