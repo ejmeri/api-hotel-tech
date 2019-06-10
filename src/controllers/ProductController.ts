98fdee393d9e80dce491140d1f5a39824b4c6bcb
ejmeri <elmerisilva@hotmail.com>
2019-06-10 19:12:38 -0300

    atualizaçoes produto

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,14 +1,44 @@
 import { Application, Router, Request, Response } from "express";
 import { injectable, inject } from "inversify";
+import { TYPES } from '../config/types';
+import { ProductService } from "../business/products/services/ProductService";
+import { Product } from '../business/products/models/Product';
+import { ProductBedroom } from '../business/products/models/ProductBedroom';
 
 
 @injectable()
 export class ProductController {
 
+    @inject(TYPES.ProductService)
+    private readonly productService: ProductService;
+
     public register(app: Application): void {
-        app.route('/product')
+        app.route('/products')
             .get((req: Request, res: Response) => {
-                res.send('Olá Mundo');
+                this.productService.findAll()
+                    .then(ret => res.send(ret))
+                    .catch(err => res.send(err).status(401));
+            })
+
+            .post((req: Request, res: Response) => {
+                this.productService.save(req.body as Product)
+                    .then(ret => res.send(ret))
+                    .catch(err => res.send(err).status(401));
+            });
+
+
+        app.route('/products/bedrooms/:id')
+            .get((req: Request, res: Response) => {
+                this.productService.findByBedroom(req.params.id)
+                    .then(ret => res.send(ret))
+                    .catch(err => res.send(err).status(401));
+            });
+
+        app.route('/products/bedrooms')
+            .post((req: Request, res: Response) => {
+                this.productService.saveProductBedroom(req.body as ProductBedroom)
+                    .then(ret => res.send(ret))
+                    .catch(err => res.send(err).status(401));
             });
     }
 }
